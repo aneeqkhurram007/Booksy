bec5400fe1fbff8a041ddd5d8b5977e72d5ca0bc
Aneeq Khurram <aneeqkhurram007@yahoo.com>
2022-04-29 22:26:41 +0500

    books component added

--- src/components/BookCards.js
+++ src/components/BookCards.js
@@ -1,14 +1,40 @@
+import { Card, Row, Col, Button } from 'antd'
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const BookCards = ({ books }) => {
+    const { userType } = useSelector(state => state.user)
     return (
-        <ul>{
-            books.map(book => (
-                <li key={book.id}>
-                    {book.title}
-                </li>
-            ))
-        }</ul>
+        <Row justify='space-between px-2'>
+            <div>{
+                books.map(book => (
+                    <Col key={book.id} className="my-4">
+                        <Card className='border rounded-lg flex flex-col justify-center items-center' size="default" bordered={true} >
+                            <img className='w-44 h-44 mx-auto' src={book?.imageDownloadUrl} />
+                            <h4 className="text-lg font-semibold">
+                                {book.title}
+                            </h4>
+                            <div className='flex justify-between space-x-2'>
+                                <p className="text-base font-semibold">Language:
+                                    <span className='font-semibold text-green-500'>{book?.language}</span></p>
+                                <p className="text-base font-semibold">Genre:
+                                    <span className='font-semibold text-green-500'>{book?.genre}</span></p>
+                            </div>
+                            {userType === "User" && (
+
+                                <div className='flex justify-between mt-4'>
+                                    <Button size='small' className='bg-blue-400' type="primary"><a href={book?.fileDownloadUrl}>Download Book</a></Button>
+                                    <Button size='small' className='bg-orange-400' type="primary">Leave a Review</Button>
+                                </div>
+
+                            )}
+
+                        </Card>
+                    </Col>
+
+                ))
+            }</div>
+        </Row>
     )
 }
 
